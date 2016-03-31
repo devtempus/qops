30441c4dedfcbe9fa4dd2a99674cf44cf154e487
Sergey Doryba <golfersds@gmail.com>
2016-03-31 11:41:47 +0300

    Basic Setup Admin Functional

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def twitterized_type(type)
+    case type.to_sym
+      when :alert
+        'warning'
+      when :error
+        'danger'
+      when :notice
+        'info'
+      when :success
+        'success'
+      else
+        type.to_s
+    end
+  end
 end
