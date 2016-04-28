45275861ae585ebf0bb7af2379dab27ac8663003
Sergey Doryba <golfersds@gmail.com>
2016-04-29 00:18:56 +0300

    Added template  for tag functional

---
+++ app/controllers/admin/tags_controller.rb
@@ -0,0 +1,20 @@
+class Admin::TagsController < Admin::AdminController
+  respond_to :html, :js
+  def show
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def published
+  end
+end
