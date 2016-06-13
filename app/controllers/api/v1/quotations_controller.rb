d6d1d58981c982a3e2e04e442838cf93379f1247
Sergey Doryba <golfersds@gmail.com>
2016-06-13 17:35:29 +0300

    A lot fixes

--- app/controllers/api/v1/quotations_controller.rb
+++ app/controllers/api/v1/quotations_controller.rb
@@ -9,36 +9,48 @@ module Api
       end
 
       def create
-        return if params[:quotations].empty?
-        quotations = JSON.parse(params[:quotations])
-        Quotation.transaction do
-          author = request_author(JSON.parse(params[:author]))
-
-          @quotations = quotations.each do |quotation_params|
-            create_quotation(quotation_params.deep_symbolize_keys, author)
-          end
-        end
-        respond_with @quotations
+        return if params[:quotation].blank? || params[:author].empty?
+        params_author = JSON.parse(params[:author]).deep_symbolize_keys unless params[:author].nil?
+        author = request_author(params_author[:full_name])
+        quotation = create_quotation(params[:quotation], author)
+        categories = request_categories params_author[:categories]
+        tags = request_tags(JSON.parse params[:tags])
+        data_response =
+            {
+                author: author,
+                quotation: quotation,
+                tags: tags.map(&:id),
+                categories: categories.map(&:id)
+            }
+        respond_with data_response, location: root_path
       end
 
       private
 
-      def create_quotation(quotation, author)
-        q_option = quotation.deep_symbolize_keys
-        Quotation.create author_id: author.id, full_text: q_option[:full_text]
+      def create_quotation(quotation_text, author)
+        return if quotation_text.blank?
+        Quotation.find_or_create_by author_id: author.id, full_text: quotation_text
       rescue => e
         fail "Create quotation Error! #{e.message}"
       end
 
-      def request_author(author)
-        option = author.deep_symbolize_keys
-        author = unless Author.find_by_full_name(option[:full_name])
-          Author.create full_name: option[:full_name], description: option[:description],
-                        die_date: option[:die_date], burn_date: option[:burn_date]
-        end
-        author
+      def request_author(author_name)
+        Author.find_or_create_by full_name: author_name
       rescue => e
         fail "Author Error! #{e.message}"
+      end
+
+      def request_tags(tags)
+        tags.map{|tag| Tag.find_or_create_by(name: tag)}
+      rescue => e
+        fail "Tag Error! #{e.message}"
+      end
+
+      def request_categories(categories)
+        categories = ['none_category'] if categories.empty?
+        categories.map{ |category| Category.find_or_create_by name: category }
+      rescue => e
+        fail "Category Error! #{e.message}"
       end
 
       def quotation_params
