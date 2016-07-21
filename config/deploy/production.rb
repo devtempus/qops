fe2d1d549f633c391d35ca6ef545636142c4429f
Sergey Doryba <golfersds@gmail.com>
2016-07-21 17:44:03 +0300

    Prepare to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+set :stage, :production
+server '95.46.98.193', user: 'deploy', roles: %w{web app}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
