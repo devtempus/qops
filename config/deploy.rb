a1a22f0bedc9817b75dbfc4b8b76d51ddfda22d5
Sergey Doryba <golfersds@gmail.com>
2016-08-30 23:07:42 +0300

    added clean rake task for capistrano task

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,6 +60,18 @@ namespace :deploy do
     end
   end
 
+  desc 'Resets DB without create/drop'
+  task :reset_db do
+    on primary :db do
+      within release_path do
+        with rails_env: fetch(:stage) do
+          execute :rake, 'db:schema:load'
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
   before :publishing, 'deploy:create_db'
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
