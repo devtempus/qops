2a4596968326de134071d6f5209092df8a9a576a
Sergey Doryba <golfersds@gmail.com>
2016-08-30 23:08:54 +0300

    Added render list of quotation on dashboard page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def index
+    @quotations = Quotation.publicated.paginate(page: params[:page], per_page: Admin::AdminController::PER_PAGE)
   end
 
   def show
