d4157b521e75e108ca5271e286e54b1bc479a36d
Sergey Doryba <golfersds@gmail.com>
2016-09-19 13:58:14 +0300

    Refactored logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,38 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  # before_action :configure_permitted_parameters, if: :devise_controller?
-  # after_filter :return_errors, only: [:page_not_found, :server_error]
 
-
-  PER_PAGE = 10
+  PER_PAGE = 5
 
   rescue_from CanCan::AccessDenied do |exception|
     Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
     flash[:error] = exception.message
     redirect_to root_path
   end
-
-
-  # def page_not_found
-  #   @status = 404
-  #   @layout = "application"
-  #   @template = "not_found_error"
-  # end
-  #
-  # def server_error
-  #   @status = 500
-  #   @layout = "error"
-  #   @template = "internal_server_error"
-  # end
-
-  private
-
-  def return_errors
-    respond_to do |format|
-      format.html { render template: 'errors/' + @template, layout: 'layouts/' + @layout, status: @status }
-      format.all  { render nothing: true, status: @status }
-    end
-  end
-
 end
--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,8 @@
 class DashboardsController < ApplicationController
   def index
-    @quotations = Quotation.publicated.paginate(page: params[:page], per_page: ApplicationController::PER_PAGE)
+    @quotations ||= Quotation.publicated
+                        .paginate page: params[:page],
+                                  per_page: ApplicationController::PER_PAGE
   end
 
   def show
