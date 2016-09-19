6ef4f6ae2f69e6363b6e4eb8371dd65389b1fafb
Sergey Doryba <golfersds@gmail.com>
2016-09-19 13:53:04 +0300

    Added page And settings template

---
+++ app/controllers/admin/pages_controller.rb
@@ -0,0 +1,21 @@
+module Admin
+  class PagesController < Admin::BaseAdminController
+    def index
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def public
+    end
+
+    def faq
+    end
+  end
+end
---
+++ app/controllers/admin/settings_controller.rb
@@ -0,0 +1,21 @@
+module Admin
+  class SettingsController < Admin::BaseAdminController
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+  end
+end
